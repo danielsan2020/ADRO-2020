bd529a360ec2486ba879ecc90aac561a8c8b3d70
danielsan2020 <danielsancheztaza@gmail.com>
2020-03-24 13:43:16 -0600

    actualización de php

--- php/contacto.php
+++ php/contacto.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_POST["submit"]) {
-    $recipient="hola@mail.com";
+    $recipient="info@adrosl.com";
     $subject="Form to email message";
     $sender=$_POST["sender"];
     $senderPhone=$_POST["senderPhone"];
@@ -10,44 +10,45 @@ if($_POST["submit"]) {
     $message=$_POST["message"];
 
     $senderMeet = $_POST['senderMeet'];
-    switch ($visitor) {
+    switch ($senderMeet) {
         case 'senderMeet1':
-        $mail_to = hola@mail.com';
-        break;
+        $mail_to = 'info@adrosl.com';
+        break; 
     case 'senderMeet2':
-        $mail_to = hola@mail.com';
+        $mail_to = 'info@adrosl.com';
         break;
     case 'senderMeet3':
-        $mail_to = hola@mail.com';
+        $mail_to = 'info@adrosl.com';
         break;
     case 'senderMeet4':
-        $mail_to = hola@mail.com';
+        $mail_to = 'info@adrosl.com';
         break;
 }
 
-$senderSubject = $_POST['senderSubject'];
-switch ($visitor) {
-    case 'senderSubject1':
-    $mail_to = hola@mail.com';
-        break;
-    case 'senderSubject2':
-    $mail_to = hola@mail.com';
-        break;
-    case 'senderSubject3':
-    $mail_to = hola@mail.com';
-        break;
-    case 'senderSubject4':
-    $mail_to = hola@mail.com';
-        break;
-    case 'senderSubject5':
-    $mail_to = hola@mail.com';
-        break;
-    case 'senderSubject6':
-    $mail_to = hola@mail.com';        
-        break;
+    $senderSubject = $_POST['senderSubject'];
+    switch ($senderMeet) {
+        case 'senderSubject1':
+        $mail_to = 'info@adrosl.com';
+            break;
+        case 'senderSubject2':
+        $mail_to = 'info@adrosl.com';
+            break;
+        case 'senderSubject3':
+        $mail_to = 'info@adrosl.com';
+            break;
+        case 'senderSubject4':
+        $mail_to = 'info@adrosl.com';
+            break;
+        case 'senderSubject5':
+        $mail_to = 'info@adrosl.com';
+            break;
+        case 'senderSubject6':
+        $mail_to = 'info@adrosl.com';        
+            break;
 }
 
-senderSubject1
+// es correcto este sender subject1, no le encuentro sentido
+senderSubject1 
 
     $mailBody="Name: $sender\n Phone: $senderPhone\n City: $senderCity\n Email: $senderEmail\n Meet: $senderMeet\n Subject: $senderSubject\n Message: $message\n";
 
